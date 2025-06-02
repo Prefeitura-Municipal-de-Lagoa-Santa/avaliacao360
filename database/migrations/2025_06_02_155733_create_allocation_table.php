1df8ce569664b4f51dd19654260e9abd81d45a99
Ueliton <ueliton_vieira@hotmail.com>
2025-06-02 13:31:47 -0300

    criaçao tabela allocations e remoção dos campos da tabela usuario.

---
+++ database/migrations/2025_06_02_155733_create_allocation_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('allocations', function (Blueprint $table) {
+            $table->id();
+            $table->string('allocation_code')->nullable(); // LOTACAO_CODIGO (originalmente LOTACAO)
+            $table->string('allocation_name')->nullable(); // LOTACAO_NOME (originalmente NOME_LOT)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('allocations');
+    }
+};
