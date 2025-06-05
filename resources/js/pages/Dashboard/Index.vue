f5ed6858edd5bbd5da52a3211f6aeb7fe99197a1
Ueliton <ueliton_vieira@hotmail.com>
2025-06-05 11:18:30 -0300

    configurando dashboard, fundo azul, botão sair e menu offcanvas

--- resources/js/pages/Dashboard/Index.vue
+++ resources/js/pages/Dashboard/Index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import DashboardLayout from '@/layouts/DashboardLayout.vue';
-import DashboardCard from '@/Components/DashboardCard.vue';
+import DashboardCard from '@/components/DashboardCard.vue';
 import { Head } from '@inertiajs/vue3'; // Para gerenciar o <head> da página
+import * as icons from 'lucide-vue-next';
 
 // Importar SVGs como componentes ou usar inline SVG
 // Exemplo de como poderia ser um SVG inline ou importado
@@ -9,7 +10,7 @@ const CheckCircleIcon = `<svg class="w-8 h-8" fill="none" stroke="currentColor" 
 const ClockIcon = `<svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>`;
 const ChartBarIcon = `<svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 12l3-3 3 3 4-4M18 14H6M2 5h20a1 1 0 011 1v12a1 1 0 01-1 1H2a1 1 0 01-1-1V6a1 1 0 011-1z"></path></svg>`; // Ícone de exemplo, pode ser melhorado
 const CalendarIcon = `<svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>`;
-
+const EvalutionIcon = `<svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M17 16l-4 4m0 0l-4-4m4 4V8"></path></svg>`;
 
 // Dados para os cards (poderiam vir do controller Laravel via props)
 const dashboardData = {
@@ -33,11 +34,13 @@ function showDetailsForDeadline() {
   <Head title="Dashboard" />
   <DashboardLayout pageTitle="Dashboard de Avaliação">
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      
       <DashboardCard
         :value="dashboardData.completedAssessments"
         label="Avaliações Concluídas"
         iconBgColor="#10b981"
-        chartPlaceholderText="Gráfico de Conclusão"
+        :buttonAction="showDetailsForDeadline"
+        buttonText="Visualizar"
       >
         <template #icon>
           <div v-html="CheckCircleIcon"></div>
@@ -48,10 +51,13 @@ function showDetailsForDeadline() {
         :value="dashboardData.pendingAssessments"
         label="Avaliações Pendentes"
         iconBgColor="#f97316"
-        chartPlaceholderText="Gráfico de Pendências"
+        :buttonAction="showDetailsForDeadline"
+        buttonText="Ver Pendências"
       >
         <template #icon>
-          <div v-html="ClockIcon"></div>
+          <icons.CircleAlert>
+          </icons.CircleAlert>
+         
         </template>
       </DashboardCard>
 
@@ -59,28 +65,28 @@ function showDetailsForDeadline() {
         :value="dashboardData.overallProgress"
         label="Progresso Geral"
         iconBgColor="#6366f1"
-        chartPlaceholderText="Gráfico de Progresso"
+        :buttonAction="showDetailsForDeadline"
+        buttonText="Ver Progresso"
       >
         <template #icon>
-          <div v-html="ChartBarIcon"></div>
+          <icons.ChartNoAxesCombined>
+          </icons.ChartNoAxesCombined>
         </template>
       </DashboardCard>
 
       <DashboardCard
         :value="dashboardData.nextDeadline"
-        label="Próximo Prazo"
+        label="Prazo"
         iconBgColor="#ef4444"
         buttonText="Ver Detalhes"
         :buttonAction="showDetailsForDeadline"
       >
         <template #icon>
-          <div v-html="CalendarIcon"></div>
+          <icons.CalendarDays>
+          </icons.CalendarDays>
         </template>
       </DashboardCard>
     </div>
   </DashboardLayout>
 </template>
 
-<style scoped>
-/* Estilos específicos para a página do dashboard, se necessário */
-</style>
