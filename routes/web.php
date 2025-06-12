74d042ac8457a82c74086e0b25581bad08148eaa
LeandroLDomingos <leandrolucasdomingos15@outlook.com>
2025-06-12 10:06:18 +0000

    fix: Forms Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FormController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\EnsureCpfIsFilled;
 use Illuminate\Support\Facades\Route;
