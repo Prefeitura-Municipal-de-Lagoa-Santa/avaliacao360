8f1b6384c39d1c1f85c5526ca2c74911e30698ce
Ueliton <ueliton_vieira@hotmail.com>
2025-10-07 11:47:56 -0300

    atualizado a pedido do Daniel

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use App\Console\Commands\SendEvaluationNotifications;
 use App\Console\Commands\AutoGenerateEvaluations;
+use App\Console\Commands\RecalculateQuestionWeights;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
@@ -21,3 +22,10 @@ Schedule::command(SendEvaluationNotifications::class)->dailyAt('08:00');
 
 // Agendamento automático diário de geração de avaliações (às 06:00)
 Schedule::command(AutoGenerateEvaluations::class)->dailyAt('06:00');
+
+// Comando para recalcular pesos das questões
+Artisan::command('forms:recalculate-weights {--form-id= : ID específico do formulário}', function () {
+    $this->call(RecalculateQuestionWeights::class, [
+        '--form-id' => $this->option('form-id')
+    ]);
+})->purpose('Recalcula os pesos das questões dos formulários');
